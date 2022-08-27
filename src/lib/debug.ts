c6b9a94002e20be9edde79d7270c00412e99f956
Bahman Asheghi <Asheghi.sh@gmail.com>
2022-08-27 19:42:49 +0430

    cleaner output for test env

--- src/lib/debug.ts
+++ src/lib/debug.ts
@@ -1,6 +1,21 @@
 import DebugLevel from "debug-level";
 
 export function getLogger(identifier) {
+  if (process.env.NODE_ENV === "test") {
+    const dummy = () => {
+      ("");
+    };
+
+    return {
+      fatal: dummy,
+      error: dummy,
+      warn: dummy,
+      info: dummy,
+      debug: dummy,
+      trace: dummy,
+      log: dummy,
+    };
+  }
   const { fatal, error, warn, info, debug, trace, log } = DebugLevel(
     `app:${identifier}`,
     {}
