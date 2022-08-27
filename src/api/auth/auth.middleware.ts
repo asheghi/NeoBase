e27b7b58d73eb5641f3b9b0f070cf602c3ac6ee4
Bahman Asheghi <Asheghi.sh@gmail.com>
2022-08-28 01:07:35 +0430

    added test for auth middleware

--- src/api/auth/auth.middleware.ts
+++ src/api/auth/auth.middleware.ts
@@ -1,31 +1,41 @@
-import { NextFunction, Response } from "express";
-import { getCollection } from "../../lib/db/connector";
+import { NextFunction, Response, Request } from "express";
+import { getAuthCollection  } from "../../lib/db/connector";
 import { getLogger } from "../../lib/debug";
 import { extractToken } from "../../lib/jwt-utils";
+import { UserType } from "../../types/user.type";
 
 const log = getLogger("auth.middleware");
-
+/**
+ * express middleware which attemps to authenticate request
+ */
 export const authenticateUserRequest = async (
-  req: any,
+  _req: Request,
   res: Response,
   next: NextFunction
 ) => {
+  const req = _req as Request & { user: UserType; project: string | undefined };
   if (req.user && req.user._id) return next();
   try {
-    const token = req.headers["x-auth-token"];
+    const token = req.headers["x-auth-token"] as string | undefined;
+    if (!token) return next();
     const { email } = extractToken(token);
-    const Users = await getCollection("auth", req.project);
+    if (!req.project) throw new Error("project was not set on request.");
+    const Users = await getAuthCollection(req.project);
     req.user = await Users.findOne({ email });
     req.user.auth_provider = "auth";
-  } catch (e: any) {
+  } catch (e) {
     log.debug("failed to authenticate", e.message);
   }
   return next();
 };
-
-export async function authGuard(req: any, res: Response, next: NextFunction) {
-  if (!req.user) {
-    return res.status(401).send();
-  }
+/**
+ * express middlewares which block un-authenticated request
+ */
+export async function authGuard(
+  req: Request & { user?: UserType },
+  res: Response,
+  next: NextFunction
+) {
+  if (!req.user) return res.status(401).send();
   return next();
 }
---
+++ src/test/api/auth/auth.middleware.test.ts
@@ -0,0 +1,128 @@
+import { Response } from "express";
+import {
+  authenticateUserRequest,
+  authGuard,
+} from "../../../api/auth/auth.middleware";
+import { UserType } from "../../../types/user.type";
+
+const user: UserType = {
+  email: "valid@mail.com",
+  role: "admin",
+  _id: "asdf",
+  auth_provider: "account",
+};
+
+jest.mock("../../../lib/jwt-utils.ts", () => {
+  return {
+    extractToken: (token) => {
+      if (token === "valid-token") return { email: user.email };
+      throw new Error("invalid token");
+    },
+  };
+});
+
+jest.mock("../../../lib/db/connector", () => {
+  return {
+    getAuthCollection: async () => {
+      return {
+        findOne: async (filter) => {
+          if (filter.email && filter.email === user.email) return user;
+          return null;
+        },
+      };
+    },
+  };
+});
+
+describe("Auth Middlewares", () => {
+  describe("AuthenticateRequest", () => {
+    describe("given req has no header", () => {
+      it("it should call next", () => {
+        const req: any = { headers: {}, project: "test" };
+        const res = {} as Response;
+        const next = jest.fn();
+        authenticateUserRequest(req, res, next);
+        expect(next).toHaveBeenCalled();
+      });
+    });
+
+    describe("given req is already authenticated", () => {
+      it("should call next", () => {
+        const req: any = {
+          user,
+          headers: {},
+          project: "test",
+        };
+        const res = {} as Response;
+        const next = jest.fn();
+        authenticateUserRequest(req, res, next);
+        expect(next).toHaveBeenCalled();
+      });
+    });
+
+    describe("given the req header is invalid", () => {
+      it("it should call next", () => {
+        const req: any = {
+          headers: {
+            "x-auth-token": "invalid-token",
+          },
+          project: "test",
+        };
+        const res = {} as Response;
+        const next = jest.fn();
+        authenticateUserRequest(req, res, next);
+        expect(next).toHaveBeenCalled();
+      });
+    });
+
+    describe("given the req has valid header", () => {
+      it("should call next", async () => {
+        const req: any = {
+          headers: {
+            "x-auth-token": "valid-token",
+          },
+          project: "test",
+        };
+        const res = {} as Response;
+        const next = jest.fn();
+        await authenticateUserRequest(req, res, next);
+        // expect(AccountsService.findUserByEmail).toBeCalledWith(user.email);
+        expect(req.user).toEqual(user);
+        expect(next).toHaveBeenCalled();
+      });
+    });
+  }); // authentiate request
+  describe("Guard", () => {
+    describe("given requst is authenticated", () => {
+      it("should call next", () => {
+        const req: any = {
+          user,
+          project: "test",
+        };
+        const res: any = {};
+        const next = jest.fn();
+        authGuard(req, res, next);
+        expect(next).toBeCalled();
+      });
+    });
+    describe("given un-authenticated request", () => {
+      it("should return 401", () => {
+        const req: any = {
+          project: "test",
+        };
+        const send = jest.fn();
+        const res: any = {
+          status: jest.fn().mockImplementation(() => {
+            return {
+              send,
+            };
+          }),
+        };
+        const next = jest.fn();
+        authGuard(req, res, next);
+        expect(res.status).toBeCalledWith(401);
+        expect(send).toBeCalled();
+      });
+    });
+  });
+});
