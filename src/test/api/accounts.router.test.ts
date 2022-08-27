b7fbd2a1de20e2a3893a2ddd1bca3be0dc610110
Bahman Asheghi <Asheghi.sh@gmail.com>
2022-08-27 17:11:59 +0430

    updated accounts router tests

--- src/test/api/accounts.router.test.ts
+++ src/test/api/accounts.router.test.ts
@@ -18,8 +18,126 @@ jest.mock("../../api/accounts/accounts.middleware", () => {
   };
 });
 
+const reqBody = {
+  email: "valid@email.com",
+  password: "password",
+};
+let app;
+
 describe("Accounts Router", () => {
-  let app;
+  describe("register", () => {
+    describe("given new user with valid payload", () => {
+      it("should create user and return token", async () => {
+        const res = await app.post("/register").send(reqBody);
+        const { token } = res.body;
+        expect(res.statusCode).toBe(200);
+        expect(token).toBeTruthy();
+        expect(typeof token).toBe("string");
+
+        expect(AccountsService.register).toBeCalledWith(
+          reqBody.email,
+          reqBody.password
+        );
+      });
+
+      describe("given user already registered", () => {
+        it("should return 422 error", async () => {
+          const res = await app
+            .post("/register")
+            .send({ ...reqBody, email: "existing@mail.com" });
+
+          expect(res.statusCode).toBe(422);
+
+          expect(AccountsService.register).toBeCalledWith(
+            "existing@mail.com",
+            reqBody.password
+          );
+        });
+      });
+    }); // valid payload
+
+    describe("given no payload", () => {
+      it("should return validation error", (done) => {
+        app.post("/register").then((res) => {
+          expect(res.statusCode).toBe(400);
+          expect(Array.isArray(res.body)).toBeTruthy();
+          expect(Object.keys(res.body[0]).includes("message")).toBeTruthy();
+          done();
+        });
+      });
+    }); // no payload
+
+    describe("given invalid payload", () => {
+      it("should return validation error", (done) => {
+        app
+          .post("/register")
+          .send({ email: "not_a_email", password: "short" })
+          .then((res) => {
+            expect(res.statusCode).toBe(400);
+            expect(Array.isArray(res.body)).toBeTruthy();
+            expect(Object.keys(res.body[0]).includes("message")).toBeTruthy();
+            done();
+          });
+      });
+    }); // invalid payload
+  });
+
+  describe("login", () => {
+    describe("given valid credentials", () => {
+      it("should return token", async () => {
+        const res = await app.post("/login").send(reqBody);
+
+        expect(res.statusCode).toBe(200);
+        expect(res.body.token).toEqual(expect.any(String));
+
+        expect(AccountsService.login).toBeCalledWith(
+          reqBody.email,
+          reqBody.password
+        );
+      });
+      describe("given non-existing email", () => {
+        it("should return error 400", async () => {
+          const res = await app.post("/login").send({
+            email: "non.existing.user@mail.com",
+            password: reqBody.password,
+          });
+
+          expect(res.statusCode).toBe(400);
+        });
+      });
+    });
+
+    describe("given invalid credentials", () => {
+      it("should return 400", async () => {
+        const res = await app
+          .post("/login")
+          .send({ email: "invalid@mail.com", password: "invalidpass" });
+
+        expect(res.statusCode).toBe(400);
+      });
+    });
+  }); // login
+
+  describe("Me", () => {
+    describe("given valid jwt", () => {
+      it("should return payload", async () => {
+        const user = { email: "existing@mail.com" };
+        const token = AccountsService.generateToken(user);
+        const res = await app.get("/me").set("x-account-token", token);
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toBeTruthy();
+        expect(res.body.email).toBe(user.email);
+      });
+    });
+
+    describe("given invalid jwt", () => {
+      it("should return error 401", async () => {
+        const res = await app.get("/me");
+        expect(res.statusCode).toBe(401);
+      });
+    });
+  });
+
   beforeEach(() => {
     app = superRouter(AccountsRouter);
     jest
@@ -45,113 +163,5 @@ describe("Accounts Router", () => {
         if (email === "valid@email.com") return { email };
         return null;
       });
-  });
-
-  afterAll(async () => {
-    await new Promise((resolve: any) => {
-      setTimeout(() => {
-        return resolve();
-      }, 500);
-    }); // avoid jest open handle error
-  });
-
-  const reqBody = {
-    email: "valid@email.com",
-    password: "password",
-  };
-
-  describe("register", () => {
-    describe("given new user with valid params", () => {
-      it("should create user and return token", async () => {
-        const res = await app.post("/register").send(reqBody);
-        const { token } = res.body;
-        expect(res.statusCode).toBe(200);
-        expect(token).toBeTruthy();
-        expect(typeof token).toBe("string");
-
-        expect(AccountsService.register).toBeCalledWith(
-          reqBody.email,
-          reqBody.password
-        );
-      });
-    });
-
-    describe("given user already registered", () => {
-      it("should return 422 error", async () => {
-        const res = await app
-          .post("/register")
-          .send({ ...reqBody, email: "existing@mail.com" });
-
-        expect(res.statusCode).toBe(422);
-
-        expect(AccountsService.register).toBeCalledWith(
-          "existing@mail.com",
-          reqBody.password
-        );
-      });
-    });
-
-    describe("given invalid request", () => {
-      it("should return status 400 with validation errors", (done) => {
-        app.post("/register").then((res) => {
-          expect(res.statusCode).toBe(400);
-          expect(Array.isArray(res.body)).toBeTruthy();
-          expect(Object.keys(res.body[0])).toEqual([
-            "code",
-            "expected",
-            "received",
-            "path",
-            "message",
-          ]);
-          done();
-        });
-      });
-    });
-  });
-
-  describe("login", () => {
-    describe("given valid credentials", () => {
-      it("should return token", async () => {
-        const res = await app.post("/login").send(reqBody);
-
-        expect(res.statusCode).toBe(200);
-        expect(res.body.token).toEqual(expect.any(String));
-
-        expect(AccountsService.login).toBeCalledWith(
-          reqBody.email,
-          reqBody.password
-        );
-      });
-    });
-
-    describe("given invalid credentials", () => {
-      it("should return 400", async () => {
-        const res = await app
-          .post("/login")
-          .send({ email: "invalid@mail.com", password: "invalidpass" });
-
-        expect(res.statusCode).toBe(400);
-      });
-    });
-  });
-
-  describe("Me", () => {
-    describe("given valid jwt", () => {
-      it("should return payload", async () => {
-        const user = { email: "existing@mail.com" };
-        const token = AccountsService.generateToken(user);
-        const res = await app.get("/me").set("x-account-token", token);
-        expect(res.statusCode).toBe(200);
-        expect(res.body).toBeTruthy();
-        expect(res.body.email).toBe(user.email);
-      });
-    });
-
-    describe("given invalid jwt", () => {
-      it("should return error 401", async () => {
-        const res = await app.get("/me");
-        expect(res.statusCode).toBe(401);
-      });
-    });
   });
 });
