379cd80246727a529db3b8ce83b365abdfbf6ac1
Bahman <asheghi.bm@gmail.com>
2023-04-22 21:21:34 +0200

    example minor changes

--- examples/todo-app/src/lib/client.js
+++ examples/todo-app/src/lib/client.js
@@ -1,7 +1,6 @@
 import { createClient } from "@neobase/client";
-import { getAccountToken as getToken } from "./auth";
 const baseurl = import.meta.env.VITE_BE_BASE_URL ?? "http://localhost:8080";
 
-export const Client = createClient(baseurl, { getToken });
+export const Client = createClient(baseurl);
 export const Todos = Client.Collection("todos");
 export const Auth = Client.Auth;
--- examples/todo-app/src/pages/dash/DashboardPage.vue
+++ examples/todo-app/src/pages/dash/DashboardPage.vue
@@ -1,16 +1,14 @@
 <template>
   <div class="DashboardPage">
-    <div
-      class="navbar px-10 pt-4 container mx-auto flex gap-4 py-2 text-lg px-4"
-    >
+    <div class="navbar px-10 pt-4 container mx-auto flex gap-4 py-2 text-lg px-4">
       <div class="left">
         <router-link to="/dash">Todos</router-link>
       </div>
       <div class="ml-auto flex items-center gap-4 text-gray-400">
-        <div v-text="user?.email"></div>
-        <router-link to="/login" class="text-red-800" @click="logout"
-          >Logout</router-link
-        >
+        <div v-text="user?.username"></div>
+        <router-link to="/login" class="text-red-800" @click="logout">
+          Logout
+        </router-link>
       </div>
     </div>
     <div class="nested-route-cover">
@@ -62,6 +60,7 @@ export default {
   .left {
     @apply flex items-center gap-1;
   }
+
   .nested-route-cover {
     @apply container mx-auto px-4;
   }
